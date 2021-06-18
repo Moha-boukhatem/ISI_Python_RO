db7841cd71f444d56b1d3d38bdfc5a4ae150ba16
moha-boukhatem <moha94orly@gmail.com>
2021-06-18 13:33:32 +0100

    edit Q4 funtion

--- TP4/exo1.py
+++ TP4/exo1.py
@@ -1,9 +1,11 @@
 from random import choice
 
+
 class dice:
     def __init__(self):
         self.diceNumbers = [1,2,3,4,5,6]
-        
+
+
 class coin : 
     def __init__(self):
         self.coinList = ["Face","Pile"]
@@ -25,6 +27,7 @@ def Question3():
     nbAppearance = Question2(dice().diceNumbers).count(6)
     print("Nb d'apparitions du 6  : ",nbAppearance)
 
+
 def Question4():
     
     Q4_Liste = []
@@ -37,7 +40,8 @@ def Question4():
         if nb == 6 :
             break
     print("Liste d'apparitions : ",Q4_Liste)
-    print("Nb de lancements avant obtenir 6 : ",len(Q4_Liste)-1)
+    print("Nb de lancements avant obtenir 6 : ",len(Q4_Liste))
+
 
 def Question5():
     Question2(coin().coinList)
